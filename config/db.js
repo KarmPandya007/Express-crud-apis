5964ad529c86e384435a5d684f72b1c6e7eccab4
KarmPandya007 <karmpandya007@gmail.com>
2026-02-02 17:23:53 +0530

    Added proper error catching

--- config/db.js
+++ config/db.js
@@ -7,6 +7,8 @@ const MONGO_URI = process.env.MONGO_URI;
 
 export const connectDB = async () => {
     await mongoose.connect(MONGO_URI).then(() => {
-        console.log("Database Connected")
+        console.log("✅ Database Connected")
+    }).catch((err) => {
+        console.log("❌ Database Connection Failed", err)
     })
 }
